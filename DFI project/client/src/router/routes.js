3d269650e9380c03df4e87700d6c6d55c7c7477a
hxuwaming <hxuwaming@gmail.com>
2020-12-31 20:07:23 +0800

    change map page to empty page

--- DFI project/client/src/router/routes.js
+++ DFI project/client/src/router/routes.js
@@ -7,7 +7,7 @@ import Dashboard from "@/pages/Dashboard.vue";
 import UserProfile from "@/pages/UserProfile.vue";
 import Notifications from "@/pages/Notifications.vue";
 import Icons from "@/pages/Icons.vue";
-import Maps from "@/pages/Maps.vue";
+import DBv1 from "@/pages/DBv1.vue";
 import Typography from "@/pages/Typography.vue";
 import TableList from "@/pages/TableList.vue";
 import CallAPI from "@/pages/CallAPI.vue";//Jonathan: add this
@@ -39,9 +39,9 @@ const routes = [
         component: Icons
       },
       {
-        path: "maps",
-        name: "maps",
-        component: Maps
+        path: "dbv1",
+        name: "dbv1",
+        component: DBv1
       },
       {
         path: "typography",
